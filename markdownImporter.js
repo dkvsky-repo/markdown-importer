d31cec9b982efe62e094580671e9d812e05e9734
David Kontorovsky <programatiko@pm.me>
2020-07-05 20:40:48 -0700

    Improving the script

--- markdownImporter.js
+++ markdownImporter.js
@@ -2,37 +2,43 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-function getDirectory(contentType) {
-  return path.join(process.cwd(), `markdown/${contentType}`);
+/**
+ * Get full path.
+ *
+ * @param {string} targetDirectory: A path to a given target directory in
+ *  root dir, i.e; 'markdown/resume-data', 'markdown/resume-data/projects'.
+ */
+function getFullPathTo(targetDirectory) {
+  return path.join(process.cwd(), `${targetDirectory}`);
+}
+
+function processFile(dir, fileName) {
+  const id = fileName.replace(/\.md$/, "");
+  const fullPath = path.join(dir, fileName);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const matterResult = matter(fileContents);
+  return {
+    fileName,
+    id,
+    metadata: matterResult.data,
+    content: matterResult.content,
+  };
 }
 
 /**
- * Return data from .md files.
- * 
- * @param {string} contentType: The type of content, i.e; 'pages', 'posts', etc.
- * @param {string} subDir: A subdirectory under content type, i.e; 'experiences' in 'resume/experiences'.
+ * Get data from .md files.
+ *
+ * @param {string} targetDirectory: Name of directory where content lives, i.e; 'markdown',
+ *  'markdown/pages', 'markdown/posts', etc.
  */
-function getData(contentType, subDir = undefined) {
-  let dir = getDirectory(contentType);
-  if (subDir) {
-    dir += `/${subDir}`;
-  }
+function getData(targetDirectory) {
+  let dir = getFullPathTo(targetDirectory);
 
-  const fileNames = (fs.readdirSync(dir)).filter(name => name.match(/\.md$/));
+  const fileNames = fs.readdirSync(dir).filter((name) => name.match(/\.md$/));
 
-  const data = fileNames.map(fileName => {
-    const id = fileName.replace(/\.md$/, '');
-    const fullPath = path.join(dir, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-    const matterResult = matter(fileContents);
-    return {
-      id,
-      metadata: matterResult.data,
-      content: matterResult.content
-    };
-  });
-
-  return data;
+  return fileNames.length === 1
+    ? processFile(dir, fileNames[0])
+    : fileNames.map((fileName) => processFile(dir, fileName));
 }
 
 export { getData };
